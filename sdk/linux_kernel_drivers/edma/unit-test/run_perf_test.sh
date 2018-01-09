b0fb5fa57b24220ce25b894c05ae8e1990dcf08c
Winefred Washington <winefred@amazon.com>
2018-01-09 23:24:23 +0000

    testing

--- sdk/linux_kernel_drivers/edma/unit-test/run_perf_test.sh
+++ sdk/linux_kernel_drivers/edma/unit-test/run_perf_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 readonly test_filename=perf_test.c
 readonly result_filename=perf.log
 
@@ -11,6 +13,8 @@ $(basename "$0") -- a script to compile and run performance (i.e. latency and ba
 data_size=(0x100 0x1000 0x5000)
 number_of_repetitions=(0x10 0x100 0x200)
 
+rm -f $result_filename
+
 for ((i=0;i<${#data_size[@]};i++))
 do
 	for ((j=0;j<${#number_of_repetitions[@]};j++))
@@ -20,12 +24,12 @@ do
 		gcc $test_filename -DSIZE_OF_DATA=${data_size[$i]} -DNUMBER_OF_REPETITIONS=${number_of_repetitions[$j]} -DWRITE_PERF=1 -DWRITE_PERF_VERIFY -o perf_test_write_${data_size[$i]}_${number_of_repetitions[$j]}
 
 		#clear dmesh		
-		dmesg -C
+		sudo dmesg -C
 		
 		#run test and fump dmesg
-		./perf_test_read_${data_size[$i]}_${number_of_repetitions[$j]} >> $result_filename
+		sudo ./perf_test_read_${data_size[$i]}_${number_of_repetitions[$j]} >> $result_filename
 		echo " " >> $result_filename
-		./perf_test_write_${data_size[$i]}_${number_of_repetitions[$j]} >> $result_filename
+		sudo ./perf_test_write_${data_size[$i]}_${number_of_repetitions[$j]} >> $result_filename
 		echo " " >> $result_filename
 						
 		dmesg > perf_${data_size[$i]}_${number_of_repetitions[$j]}.dmesg
--- sdk/tests/test_edma.py
+++ sdk/tests/test_edma.py
@@ -24,17 +24,17 @@ import subprocess
 import sys
 import traceback
 try:
-    import aws_fpga_utils
     import aws_fpga_test_utils
     from aws_fpga_test_utils.AwsFpgaTestBase import AwsFpgaTestBase
+    import aws_fpga_utils
 except ImportError as e:
     traceback.print_tb(sys.exc_info()[2])
-    print("error: {}\nMake sure to source hdk_setup.sh".format(sys.exc_info()[1]))
+    print("error: {}\nMake sure to source sdk_setup.sh".format(sys.exc_info()[1]))
     sys.exit(1)
 
 logger = aws_fpga_utils.get_logger(__name__)
 
-class TestSdkScripts(AwsFpgaTestBase):
+class TestEdma(AwsFpgaTestBase):
     '''
     Pytest test class.
     
@@ -47,9 +47,22 @@ class TestSdkScripts(AwsFpgaTestBase):
         Do any setup required for tests.
         '''
         AwsFpgaTestBase.setup_class(cls, __file__)
+
+        AwsFpgaTestBase.assert_sdk_setup()
+
+        assert AwsFpgaTestBase.running_on_f1_instance(), 'This test must be run on an F1 instance.'
         return
 
-    @pytest.mark.skip(reason="Not implemented")
-    def test_sdk_setup(self):
-        logger.info(self)
-        assert False
+    def test_unittest(self):
+        (agfi, afi) = self.get_agfi_from_readme('cl_dram_dma')
+        self.fpga_load_local_image(agfi, 0)
+        aws_fpga_test_utils.install_edma_driver()
+        (rc, stdout_lines, stderr_lines) = self.run_cmd("cd {}/sdk/linux_kernel_drivers/edma/unit-test && ./run_unit_test.sh".format(self.WORKSPACE), echo=True)
+        assert rc == 0
+
+    def test_perftest(self):
+        (agfi, afi) = self.get_agfi_from_readme('cl_dram_dma')
+        self.fpga_load_local_image(agfi, 0)
+        aws_fpga_test_utils.install_edma_driver()
+        (rc, stdout_lines, stderr_lines) = self.run_cmd("cd {}/sdk/linux_kernel_drivers/edma/unit-test && ./run_perf_test.sh".format(self.WORKSPACE), echo=True)
+        assert rc == 0
